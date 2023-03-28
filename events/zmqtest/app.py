1235b2b228747307ed40505148039685d7f27f70
Joe Stubbs <joestubbs5@gmail.com>
2023-03-27 19:54:10 -0500

    close to a working model build based on the full image scoring plugin.. still an issue with imports from the camera_traps_MD dir.

--- events/zmqtest/app.py
+++ events/zmqtest/app.py
@@ -1,11 +1,205 @@
+import json
+import os
+import glob
+import uuid
+from collections import OrderedDict
 import zmq
-import pyevents
+from PIL import Image
+from ctevents import ctevents
+from pyevents.events import get_plugin_socket, get_next_msg, send_quit_command
 
-def main():
+
+# get the port assigned to the Image Generating plugin
+PORT = os.environ.get('IMAGE_GENERATING_PLUGIN_PORT', 6000)
+
+
+def get_socket():
+    """
+    This function creates the zmq socket object and generates the event-engine plugin socket
+    for the port configured for this plugin.
+    """
     # create the zmq context object
     context = zmq.Context()
+    return get_plugin_socket(context, PORT)
 
 
+def get_binary(value):
+    """
+    This function is used to generate the uuid, image format and binary image and invokes the  
+    new image event.
+    """
+    uuid_image = str(uuid.uuid5(uuid.NAMESPACE_URL, value))
+    with open(value, "rb") as f:
+        binary_img = f.read()
+    img = Image.open(value)
+    img_format = img.format
+    print(f"sending new image with the following data; uuid:{uuid_image}; format: {img_format}; type(format): {type(img_format)}")
+    ctevents.send_new_image_fb_event(
+        socket, uuid_image, img_format, binary_img)
 
-if __name__ == "__main__":
+
+def simpleNext(i, value_index):
+    """
+    This function is used to retrieve the next image specified in the directory based on the timestamp
+    and invokes get binary function.
+    """
+    if i >= len(img_dict):
+        done = True
+        print(f"Hit exit condition; i: {i}; len(img_dict): {len(img_dict)}; done = {done}")
+        exit()
+    value = list(img_dict.values())[i][value_index]
+    get_binary(value)
+    # if we hit the end of the current list, move to the next time stamp
+    if value_index == len(list(img_dict.values())[i]) - 1:
+        return i+1, 0
+    return i, value_index + 1
+
+
+def burstNext(index):
+    """
+    This function send the next x (burstQuantity specified) images specified in the directory based on the timestamp
+    and invokes get binary function. 
+    """
+    # TODO -- burstNext currently produces the same behavior as simpleNext. We 
+    #         should think through how to achieve burst behavior in a simulation.
+    burst_Quantity = int(data['burstQuantity'])
+    for i in range(index, index+burst_Quantity):
+        if (i >= len(img_dict)):
+            exit()
+        value = list(img_dict.values())[i]
+        value = str(value)[1:-1]
+        get_binary(value)
+    return (index+burst_Quantity)
+
+
+def identicalTimestamp(timestamp_min):
+    """
+    Incase of multiple images with same timestamp, this function gets single image
+    and invokes get binary function.
+    """
+    if timestamp_min not in img_dict.keys():
+        print(f"Hit exit condition...timestamp_min not in img_dict.keys()")
+        exit()
+    if (len(img_dict[timestamp_min]) > 1):
+        for i in range(0, len(img_dict[timestamp_min])):
+            value = img_dict[timestamp_min][i]
+            get_binary(value)
+    return timestamp_min+start
+
+
+def nextImage(timestamp_min, index):
+    """
+    For a given static time interval(t), this fucntion gives the next image t seconds forward.
+    Binary search algorithm is used to minimize the search time.
+    """
+    # TODO -- currently, the nextImage function depends on OS timestamps on the input images, 
+    #         and therefore may not function/may give unexpected results. 
+    if index >= len(img_dict):
+        print(f"Hitting exit condition; index: {index}; len(image_dict): {len(img_dict)}")
+        
+        exit()
+    if timestamp_min > timestamp_max:
+        print(f"Hitting exit condition; timestamp_min: {timestamp_min}; timestamp_max: {timestamp_max}")
+        exit()
+    start1 = index
+    end = len(img_dict)-1
+    while start1 <= end:
+        mid = (start1 + end) // 2
+        mid_value = list(img_dict.keys())[mid]
+        if mid_value < timestamp_min:
+            start1 = mid + 1
+        else:
+            index = mid
+            end = mid - 1
+    timestamp_min1 = list(img_dict.keys())[index]
+    print("Output")
+    value = img_dict[timestamp_min1]
+    value = str(value)[1:-1]
+    get_binary(value)
+    return timestamp_min1+start, index
+
+
+def randomImage(timestamp_min, index):
+    """
+    For a given dynamic time interval(t), this fucntion gives the next image t seconds forward.
+    Binary search algorithm is used to minimize the search time.
+    """
+    if index >= len(img_dict) or timestamp_min > timestamp_max:
+        exit()
+    start1 = index
+    print(timestamp_min)
+    end = len(img_dict)-1
+    while start1 <= end:
+        mid = (start1 + end) // 2
+        mid_value = list(img_dict.keys())[mid]
+        if mid_value < timestamp_min:
+            start1 = mid + 1
+        else:
+            index = mid
+            end = mid - 1
+    timestamp_min = list(img_dict.keys())[index]
+    print("Output")
+    value = img_dict[timestamp_min]
+    value = str(value)[1:-1]
+    get_binary(value)
+    return timestamp_min, index
+
+
+print("Image Generating Plugin starting up...")
+with open('input.json') as f:
+    data = json.load(f)
+user_input = data['path']
+print(f"user_input: {user_input}")
+start = int(data['timestamp'])
+"""
+Creates an ordered dictionary with the image files in the directory.
+Future: Think of a way to minimize the memory usage
+"""
+
+list_of_files = filter(os.path.isfile, glob.glob(user_input + '/*'))
+list_of_files = sorted(list_of_files, key=os.path.getmtime)
+print(f"list_of_files: {list_of_files}")
+img_dict = OrderedDict()
+for file_name_full in list_of_files:
+    if ('.DS_Store' not in file_name_full):
+        timestamp = int(os.path.getmtime(file_name_full))
+        if timestamp in img_dict.keys():
+            img_dict[timestamp] += [file_name_full]
+        else:
+            img_dict[timestamp] = [file_name_full]
+timestamp_max = list(img_dict.keys())[len(img_dict) - 1]
+socket = get_socket()
+done = False
+
+def main():
+    timestamp_min = list(img_dict.keys())[0]
+    initial_index = 0
+    index = 0
+    indexvalue = 0
+
+    
+    while not done:
+    #for i in range(0, 100):
+        if data['callingFunction'] == "nextImage":
+            print("Timed Next")
+            timestamp_min, initial_index = nextImage(
+                timestamp_min, initial_index)
+        elif data['callingFunction'] == "burstNext":
+            print("Burst Next")
+            index = burstNext(index)
+        elif data['callingFunction'] == "identicalTimestamp":
+            timestamp_min = identicalTimestamp(timestamp_min)
+            print(timestamp_min)
+        elif data['callingFunction'] == "randomImage":
+            random_timestamp = int(
+                input("Enter the random timestamp in seconds: "))
+            timestamp_min += random_timestamp
+            timestamp_min, initial_index = randomImage(
+                timestamp_min, initial_index)
+        else:
+            print(f"Simple Next; index: {index}; indexvalue: {indexvalue} ")
+            index, indexvalue = simpleNext(index, indexvalue)
+
+
+if __name__ == '__main__':
     main()
--- model/flake.nix
+++ model/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example Events apps packaged using poetry2nix";
+  description = "Example ML model apps packaged using poetry2nix";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -16,7 +16,8 @@
         inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication mkPoetryEnv;
         pkgs = nixpkgs.legacyPackages.${system};
         shell = shell-utils.myShell.${system};
-        ai4eutils = pkgs.python310Packages.buildPythonPackage {
+        myPython = pkgs.python38;
+        ai4eutils = myPython.pkgs.buildPythonPackage {
             name = "ai4eutils";
             src = pkgs.fetchgit {
                 url = "https://github.com/microsoft/ai4eutils";
@@ -25,37 +26,88 @@
             };
             format = "other";
             installPhase = ''
-              mkdir -p $out/lib/python3.10/site-packages
-              cp -r ai4eutils $out/lib/python3.10/site-packages/
+              mkdir -p $out/lib/python3.8/site-packages/ai4eutils
+              cp -r . $out/lib/python3.8/site-packages/ai4eutils/
             '';
-
-        }
-#        ai4eutils = pkgs.stdenv.mkDerivation rec {
-#            name = "ai4eutils";
-#          src = pkgs.fetchgit {
-#                url = "https://github.com/microsoft/ai4eutils";
-#                rev = "a7aefc9cf6ff0564a83e0a1ddc903ef22561fdd5";
-#                sha256 = "sha256-w1Eid+0exFzQobLjC3Eh1UlvLgauw/FY5H4LsnER3ek=";
-#            };
-#            buildPhase = "";
-#           installPhase = ''
-#              mkdir -p $out
-#              cp -r ${src} $out
-#            '';
-#        };
+        };
+        cameraTrapsMD = myPython.pkgs.buildPythonPackage {
+            name = "camera_traps_MD"; 
+            src = pkgs.fetchFromGitHub {
+                owner = "sowbaranika1302";
+                repo = "camera_traps_MD";
+                rev = "28f91e01b2afadde23a0e653a4ab9d6879a976c9";
+                hash = "sha256-VBwprg1qvxtWBMOZpmJk6qqUhXUvmnVNqEvgoES4J5k=";
+            };
+            format = "other";
+            installPhase = ''
+              mkdir -p $out/lib/python3.8/site-packages/camera_traps_MD
+              cp -r . $out/lib/python3.8/site-packages/camera_traps_MD/
+            '';
+        };
+        yolov5 = myPython.pkgs.buildPythonPackage {
+            name = "yolov5";
+            src = pkgs.fetchFromGitHub {
+                owner = "ultralytics";
+                repo = "yolov5";
+                rev = "c23a441c9df7ca9b1f275e8c8719c949269160d1";
+                hash = "sha256-YbedVzBResnU5lwlxYkMkjqJ0f1Q48FZs+tIS1a1MUk=";
+            };
+            format = "other";
+            installPhase = ''
+              mkdir -p $out/lib/python3.8/site-packages/yolov5
+              cp -r . $out/lib/python3.8/site-packages/yolov5/
+            '';
+        };
+        mdPtModel = pkgs.fetchurl {
+          url = "https://github.com/microsoft/CameraTraps/releases/download/v5.0/md_v5a.0.0.pt";
+          sha256 = "0xmj04xwvvpqfhpvxp0j8gbkkmi98zvb8k6cs3iz4l40gklqzs4l";
+        };
+        ptModelDir = pkgs.stdenv.mkDerivation {
+          name = "ptModelDir";
+          src = self;
+          installPhase = ''
+           mkdir -p $out;
+           cp ${mdPtModel} $out/md_v5a.0.0.pt
+          '';
+        };
         myApp = mkPoetryApplication { 
+            python = myPython;
             projectDir = ./.; 
             preferWheels = true;
           };
-        newPython = pkgs.python310.withPackages (
-            ps: [ myApp ai4eutils ]
+        newPython = myPython.withPackages (
+            ps: [ myApp ai4eutils yolov5 cameraTrapsMD ]
         );
+        # newPython2 = myPython.pkgs.buildPythonApplication {
+        #   name = "newPython2";
+        #   propagatedBuildInputs = [ myApp ai4eutils yolov5 cameraTrapsMD ];
+        #   pythonPath = [ "${cameraTrapsMD}/lib/python3.8/site-packages/camera_traps_MD/" ];
+          
+        # };
+        newPython3 = pkgs.stdenv.mkDerivation {
+            name = "newPython3";
+            buildInputs = [ newPython pkgs.makeWrapper ];
+            src = self;
+            installPhase = ''
+              makeWrapper ${newPython}/bin/python $out/bin/python --set PYTHONPATH ${cameraTrapsMD}/lib/python3.8/site-packages/camera_traps_MD/:${ai4eutils}/lib/python3.8/site-packages/ai4eutils:${yolov5}/lib/python3.8/site-packages/yolov5
+            '';
+        };
+        finalBinary = pkgs.writeShellApplication {
+            name = "image_scoring_plugin";
+            runtimeInputs = [ newPython3 ptModelDir myApp ];
+            text = ''
+              cd ${ptModelDir}; ${myApp}/bin/zmqtest
+            '';
+        };
       in
       rec {
         packages = {
-          py = newPython;
+          py = finalBinary;
           default = packages.py;
           ai4eutilsPkg = ai4eutils;
+          myAppPkg = myApp;
+          modelPkg = mdPtModel;
+          pythonPkg = newPython3;
         };
 
         devShells.default = shell {
